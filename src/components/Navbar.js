5c2d7f701f8a8b74b2589505653aa585f9080880
ShashiBalu <shashibalu8@gmail.com>
2023-03-31 18:40:37 +0530

    random changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,72 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import logo from "./logo.png";
 
 const Navbar = () => {
     return (
-        <div className="navbar">
-            <NavLink activeClassName="active" exact className="nav-link" to="/">
-                Home
-            </NavLink>
-            <NavLink activeClassName="active" exact className="nav-link" to={"/collections"}>
-                Collections
-            </NavLink>
-            <NavLink activeClassName="active" exact className="nav-link" to={"/contact"}>
-                Contact
-            </NavLink>
-            <NavLink activeClassName="active" exact className="nav-link" to={"/about"}>
-                About
-            </NavLink>
-        </div>
+        <>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <img src={logo} className="logo" />
+
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="true"
+                        aria-label="Toggle navigation"
+                    >
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <NavLink
+                                    activeClassName="active"
+                                    exact
+                                    className="nav-link-box"
+                                    to="/"
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li class="nav-item">
+                                <NavLink
+                                    activeClassName="active"
+                                    exact
+                                    className="nav-link-box"
+                                    to={"/collections"}
+                                >
+                                    Collections
+                                </NavLink>
+                            </li>
+                            <li class="nav-item">
+                                <NavLink
+                                    activeClassName="active"
+                                    exact
+                                    className="nav-link-box"
+                                    to={"/contact"}
+                                >
+                                    Contact
+                                </NavLink>
+                            </li>
+                            <li class="nav-item">
+                                <NavLink
+                                    activeClassName="active"
+                                    exact
+                                    className="nav-link-box"
+                                    to={"/about"}
+                                >
+                                    About
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
     );
 };
 
